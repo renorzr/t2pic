518eada780a74a5a391fb1db79aef6ef4569e3ff
Reno Ren <friendkey@gmail.com>
2012-01-28 14:15:02 +0800

    post method supported

--- app.rb
+++ app.rb
@@ -4,8 +4,17 @@ require File.expand_path("raster", File.dirname(__FILE__))
 FONT = '/usr/share/fonts/simkai.ttf'
 
 get '/' do
+  puts 'get', params
+  proceed(params)
+end
+
+post '/' do
+  puts 'post', params
+  proceed(params)
+end
+
+def proceed(params)
   text = params[:text]
   pic = params[:pic]
   send_file(Raster.new(text, :font => FONT, :pic => pic).file)
 end
-
